5949632bf2a0389dbd6cecc379c693a43624ded3
Erik Larsson <erik.larsson@weknowit.se>
2022-11-10 14:32:29 +0100

    Started to implement assignment 2.

---
+++ lab-1/assignment-2/Lab1.2.R
@@ -0,0 +1,60 @@
+library(caret)
+library(ggplot2)
+
+# 1. Divide it into training and test data (60/40) and scale it appropriately. In the
+# coming steps, assume that motor_UPDRS is normally distributed and is a
+# function of the voice characteristics, and since the data are scaled, no
+# intercept is needed in the modelling.
+
+data = read.csv("parkinsons.csv")
+set.seed(12345)
+n = nrow(data)
+trainId = sample(1:n, floor(n*0.6))
+train = data[trainId, ]
+test = data[-trainId, ]
+
+
+scaler=preProcess(train)
+trainS=predict(scaler,train)
+testS=predict(scaler,test)
+data
+
+# 2. Compute a linear regression model from the training data, estimate training
+# and test MSE and comment on which variables contribute significantly to the model.
+
+# Comupte linear regression
+fit=lm(motor_UPDRS~., data=trainS)
+summary(fit)
+
+# Estimate training Mean Squared Error(MSE)
+train_MSE = mean((trainS$motor_UPDRS - predict.lm(fit, trainS))^2)
+
+# Estimate test MSE
+test_MSE =  mean((testS$motor_UPDRS - predict.lm(fit, testS))^2)
+
+# Which values contribute significantly to the model
+summary = summary(fit)
+coef = as.data.frame(summary$coefficients)
+coef$Estimate
+sorted_coef = coef[order(-coef$Estimate),]
+sorted_coef
+
+# 3. Implement 4 following functions by using basic R commands only (noexternal packages):
+
+# Loglikelihood
+loglikelihood = function(vector, dispersion) {
+  
+}
+
+# Ridge
+
+
+# RidgeOpt
+
+
+# DF
+
+
+
+
+
