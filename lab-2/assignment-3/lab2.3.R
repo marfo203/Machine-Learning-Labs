2cda5fdfa2397b4e15b632c8509a05344c738c47
martin <marfo203@student.liu.se>
2022-12-02 16:49:39 +0100

    assignment 3

--- lab-2/assignment-3/lab2.3.R
+++ lab-2/assignment-3/lab2.3.R
@@ -19,17 +19,29 @@ data_frame <- data.frame(data[c(1:100)]) # Stripping data of column ViolentCrime
 scaler <- preProcess(data_frame)
 dataS <- predict(scaler,data_frame)
 
+data_frame2 <- data.frame(data) 
+scaler2 <- preProcess(data_frame2)
+dataS2 <- predict(scaler,data_frame2)
+
+
 # Check mean=0
 summary(dataS)
 
 cov_matrix = cov(dataS)
 eigen_values = eigen(cov_matrix)
 
-lambda= eigen_values$values
+summary(eigen_values)
+
+lambda= eigen_values$values #lambda är lika med varieansen för en komponent
+
+eigen_values
+lambda
 
 var = sprintf("%2.3f", lambda/sum(lambda)*100) #In we add component 
+var
 
 calcVar <- function (var) {
+  sumVar = 0
   for (i in 1:100){
     sumVar = sumVar + as.numeric(var[i])
     print(sumVar)
@@ -40,13 +52,16 @@ calcVar <- function (var) {
     }
   }
 }
-sumVar = 0
+
 
 calcVar(var) #calculating hom many comonents are needed for 95%
 
 
 res=prcomp(dataS)
+?prcomp
 screeplot(res)
+plot(res)
+
 
 #What is the proportion of variation
 #explained by each of the first two principal components? 
@@ -59,12 +74,17 @@ screeplot(res)
 # Also provide a plot of the PC scores in the coordinates (PC1, PC2) in which the color of the points is given by
 # ViolentCrimesPerPop. Analyse this plot (hint: use ggplot2 package ).
 
-prin.comp <- princomp(data_frame, cor=T)
-prin.comp[1]
+prin.comp <- princomp(dataS)
+
+prin.comp
+?princomp
 #in princomp loadings is used instead of rotations used in lecture
 U <- prin.comp$loadings
+U
 U[,1]
-plot(U[,1])
+
+#plotting traceplot
+plot(sort(abs(U[,1]), decreasing = T))
 
 contribute.U <- head(sort(abs(U[,1]), decreasing = T, n = 5))
 contribute.U
@@ -72,7 +92,7 @@ contribute.U
 library(ggplot2)
 library(ggfortify)
 
-autoplot(prin.comp, data=data, colour='ViolentCrimesPerPop')
+autoplot(prin.comp, data=dataS2, colour='ViolentCrimesPerPop')
 
 # ------------------------------------------- TASK 3 ------------------------------------------------------------
 # 3. Split the original data into training and test (50/50) and scale both features and response appropriately, 
@@ -115,9 +135,52 @@ print(paste("MSE test: ", test_MSE))
 # 4. Implement a function that depends on parameter vector 𝜃 and represents the cost function for 
 # linear regression without intercept on the training data set.
 
+train.x <- as.matrix(trainS[,-101])
+train.y <- as.matrix(trainS[,101])
+
+test.x <- as.matrix(testS[,-101])
+test.y <- as.matrix(testS[,101])
+
+
+
+train.vector <- vector("numeric")
+test.vector <- vector("numeric") 
+
 costLinReg <- function(theta){
   
+  train.cost = sum( ((train.x%*%theta) - train.y )^2)/(nrow(train.x))
+  test.cost = sum( ((test.x%*%theta) - test.y )^2)/(nrow(test.x))
+  
+  test.vector<<- c(test.vector, test.cost)
+  
+  train.vector<<- c(train.vector, train.cost)
+  
+  return (train.cost);
 }
+
+print(test.vector)
+
+test.cost
+
+theta.0 <- as.matrix(rep(0,100))
+
+theta.opt <- optim(theta.0, fn=costLinReg, method="BFGS")
+
+
+plot(test.vector[-(0:500)], ylim=c(0,1), xlim=c(-5,5000))
+points(train.vector[-(0:500)], col="blue")
+abline(v=1682)
+
+which.min(test.vector)
+
+test.vector[2182]
+train.vector[2182]
+
+
+
+
+
+theta.opt$par
 
 
 
