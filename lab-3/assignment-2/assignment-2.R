3541458579d4f295eb5d641c683fc69cf2bf9575
Erik Larsson <erik.larsson@weknowit.se>
2022-12-06 23:02:03 +0100

    Started on assignment 2 task 2.

--- lab-3/assignment-2/assignment-2.R
+++ lab-3/assignment-2/assignment-2.R
@@ -18,12 +18,20 @@ te <- spam[3801:4601, ]
 
 by <- 0.3
 err_va <- NULL
+c <- NULL
 for(i in seq(by,5,by)){
   filter <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=i,scaled=FALSE)
   mailtype <- predict(filter,va[,-58])
   t <- table(mailtype,va[,58])
   err_va <-c(err_va,(t[1,2]+t[2,1])/sum(t))
+  c = c(c, i)
 }
+err_va
+c
+which.min(err_va)
+c[13]
+which.min(err_va)*by
+
 
 filter0 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE)
 mailtype <- predict(filter0,va[,-58])
@@ -55,10 +63,20 @@ t
 # Questions
 
 # 1. Which filter do we return to the user ? filter0, filter1, filter2 or filter3? Why?
+# All models use the optimal C value calculated in the for loop.
+filter0 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses the training set for training 
+filter1 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses the training set for training
+filter2 <- ksvm(type~.,data=trva,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses the training and validation set for training
+filter3 <- ksvm(type~.,data=spam,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses whole data set for training
 
+# Since we are calculating the optimal C value with the training data tr we should use a model that is trained with the same data.
+# Therefore we should use filter 0 or 1 since they are the same. 
 
 # 2. What is the estimate of the generalization error of the filter returned to the user? err0, err1, err2 or err3? Why?
 
+# err0 -> Error when training on the training set and predicting on the validation set.
+# err1 -> Error when training on the training set and predicting on the test set.
+# err2 -> Error when training on the training + validation set and predicting on the test set.
 
 # 3. Implementation of SVM predictions.
 
