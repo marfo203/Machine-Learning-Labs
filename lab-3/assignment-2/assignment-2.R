2dc2aa5af17327365e59441c78f829b6e6fbd776
Erik Larsson <erik.larsson@weknowit.se>
2022-12-08 20:39:27 +0100

    Continued with assignment 2.

--- lab-3/assignment-2/assignment-2.R
+++ lab-3/assignment-2/assignment-2.R
@@ -64,33 +64,45 @@ t
 
 # 1. Which filter do we return to the user ? filter0, filter1, filter2 or filter3? Why?
 # All models use the optimal C value calculated in the for loop.
-filter0 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses the training set for training 
-filter1 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses the training set for training
-filter2 <- ksvm(type~.,data=trva,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses the training and validation set for training
-filter3 <- ksvm(type~.,data=spam,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE) # Uses whole data set for training
+# filter0 | Uses the training set for training, testing on the validation set with error 0.0675
+# filter1 | Uses the training set for training, testing on the test set with error 0.08489388
+# filter2 | Uses the training and validation set for training, testing on the test set with error 0.082397
+# filter3 | Uses whole data set for training, testing on the test set with error 0.02122347
+
+# I think that filter 0 or filter 1 with the lowest error rate or filter 2 which is based on more training data are good candidates. I would consider filter 2 as a better 
+# candidate as it is fitted with more training data and should therefore be able to generalize better on new unseen data. 
 
 # Since we are calculating the optimal C value with the training data tr we should use a model that is trained with the same data.
 # Therefore we should use filter 0 or 1 since they are the same. 
 
 # 2. What is the estimate of the generalization error of the filter returned to the user? err0, err1, err2 or err3? Why?
 
-# err0 -> Error when training on the training set and predicting on the validation set.
-# err1 -> Error when training on the training set and predicting on the test set.
-# err2 -> Error when training on the training + validation set and predicting on the test set.
-# err3 -> Error when training on the whole data set and predicting on the test set. 
+# err0 -> Error when training on the training set and predicting on the validation set. 0.0675
+# err1 -> Error when training on the training set and predicting on the test set. This seems worse than filter 0 but it is the same model tested on a new data set. 0.08489388
+# err2 -> Error when training on the training + validation set and predicting on the test set. It is slightly better than filter 0/1, probably because the model is trained on more data. 0.082397
+# err3 -> Error when training on the whole data set and predicting on the test set. The error is quite low since we train the model on the whole data set and then use that data to test the model. 0.02122347
 
 # 3. Implementation of SVM predictions.
-
+?alphaindex
 sv<-alphaindex(filter3)[[1]]
+sv
 co<-coef(filter3)[[1]]
+co
 inte<- - b(filter3)
+inte
 k<-NULL
 for(i in 1:10) { # We produce predictions for just the first 10 points in the dataset.
   k2<-NULL
   for(j in 1:length(sv)) {
-    k2<- # Your code here
+    k2<- c(k2, co[j] * sv[i] )
   }
-  k<-c(k, # Your code here)
+  k<-c(k, sum(k2) + inte)
 }
 k
+?spam
+?kernel
+?unlist
+?apply
 predict(filter3,spam[1:10,-58], type = "decision")
+
+
