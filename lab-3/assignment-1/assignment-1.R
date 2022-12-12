c9e504a3451c53f4361e6f20d9758f1832b1935b
Erik Larsson <erik.larsson@weknowit.se>
2022-12-12 13:59:48 +0100

    Assignment 1 setup.

---
+++ lab-3/assignment-1/assignment-1.R
@@ -0,0 +1,15 @@
+# Assignment 1 - Kernel Methods
+
+# Lecture 3a, Kernel classification.
+
+# Kernel 1
+# The first to account for the physical distance from a station to the point of interest.
+# For this purpose, use the function distHaversine from the R package geosphere.
+
+# Kernel 2
+# The second to account for the distance between the day a temperature measurement
+# was made and the day of interest.
+
+# Kernel 3
+# The third to account for the distance between the hour of the day a 
+# temperature measurement was made and the hour of interest.
