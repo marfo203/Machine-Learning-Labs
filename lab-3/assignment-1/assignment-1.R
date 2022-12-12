bed9a5c0712e2cc6b2414f294d22af5fb49e9c7d
Erik Larsson <erik.larsson@weknowit.se>
2022-12-12 14:27:45 +0100

    Moved data files to the correct dir. Fixed import error with UTF-8.

--- lab-3/assignment-1/assignment-1.R
+++ lab-3/assignment-1/assignment-1.R
@@ -2,9 +2,29 @@
 
 # Lecture 3a, Kernel classification.
 
+set.seed(1234567890)
+library(geosphere)
+stations <- read.csv("stations.csv", header = TRUE)
+?read.csv()
+temps <- read.csv("temps50k.csv", header = TRUE)
+st <- merge(stations,temps,by="station_number")
+h_distance <- # These three values are up to the students
+  h_date <-
+  h_time <-
+  a <- 58.4274 # The point to predict (up to the students)
+b <- 14.826
+date <- "2013-11-04" # The date to predict (up to the students)
+times <- c("04:00:00", "06:00:00", ..., "24:00:00")
+temp <- vector(length=length(times))
+# Students’ code here
+plot(temp, type="o")
+
+
 # Kernel 1
 # The first to account for the physical distance from a station to the point of interest.
 # For this purpose, use the function distHaversine from the R package geosphere.
+
+
 
 # Kernel 2
 # The second to account for the distance between the day a temperature measurement
@@ -13,3 +33,7 @@
 # Kernel 3
 # The third to account for the distance between the hour of the day a 
 # temperature measurement was made and the hour of interest.
+
+# Summing Kernels
+
+# Multiplying Kernels
