0aa13056bdc22b816f9b66a60e19ebcb41b3602b
Erik Larsson <erik.larsson@weknowit.se>
2022-12-12 14:54:02 +0100

    Continued on assignment 1.

--- lab-3/assignment-1/assignment-1.R
+++ lab-3/assignment-1/assignment-1.R
@@ -4,27 +4,25 @@
 
 set.seed(1234567890)
 library(geosphere)
-stations <- read.csv("stations.csv", header = TRUE)
+stations <- read.csv("stations.csv", header = TRUE, fileEncoding = "latin1")
 ?read.csv()
 temps <- read.csv("temps50k.csv", header = TRUE)
 st <- merge(stations,temps,by="station_number")
-h_distance <- # These three values are up to the students
-  h_date <-
-  h_time <-
-  a <- 58.4274 # The point to predict (up to the students)
-b <- 14.826
+h_distance <- 1 # These three values are up to the students
+h_date <- 1 # These three values are up to the students
+h_time <- 1 # These three values are up to the students
+a <- 58.4274 # The point to predict (up to the students)
+b <- 14.826 # The point to predict (up to the students)
 date <- "2013-11-04" # The date to predict (up to the students)
-times <- c("04:00:00", "06:00:00", ..., "24:00:00")
+times <- c("04:00:00", "06:00:00", "08:00:00", "10:00:00", "12:00:00", "14:00:00", 
+           "16:00:00", "18:00:00", "20:00:00", "22:00:00", "24:00:00")
 temp <- vector(length=length(times))
 # Students’ code here
 plot(temp, type="o")
 
-
 # Kernel 1
 # The first to account for the physical distance from a station to the point of interest.
 # For this purpose, use the function distHaversine from the R package geosphere.
-
-
 
 # Kernel 2
 # The second to account for the distance between the day a temperature measurement
